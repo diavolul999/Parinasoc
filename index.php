7fd51ab3f853579257c1ba2f5574249f932c6530
Zaharia Raul <raul_zaharia2006@yahoo.com>
2015-10-07 18:09:09 +0300

    07.10.2015 18:10

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html5>
+<html style="background-color: steelblue;">
+
+<head>
+    <title>Asociatia parintilor</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Asociatia parintilor">
+    <meta name="author" content="Raul">
+    <link rel="stylesheet" href="style/style.css" />
+    <script src="javascript/script.js"></script>
+    <?php include 'php/php.php';?>
+</head>
+
+<body>
+    <div class="top">
+        <div class="headerest">
+            <h1 class="page-header">Asociatia parintilor</h1>
+            <!--<p class="intro-text">Acesta este o descriere!<br>Cea mai smechera descriere!</p>-->
+        </div>
+        <nav class="navbar navbar-custom navbar-fixed-top" role="navigation"  style="text-align: center;">
+            <div class="menu-container">
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                    <ul class="nav">
+                        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                        <li class="hidden" style="display: none;">
+                            <a href="#page-top">Sus</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#despre">Despre</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#informatii">Informatii</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#contact">Contact</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#logare">Logare</a>
+                        </li>
+                    </ul>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+    </div>
+    <div class="middle">
+        da
+    </div>
+</body>
+
+<footer>
+
+</footer>
+
+</html>
